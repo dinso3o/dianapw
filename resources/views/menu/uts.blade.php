5f23712632fb63c5678522d7168515165c0a3628
Diana Indah <dianaindahh0208@gmail.com>
2026-07-10 20:24:25 +0700

    perbaikan uts

--- resources/views/menu/uts.blade.php
+++ resources/views/menu/uts.blade.php
@@ -1,6 +1,5 @@
 @extends('layout.app')
 
 @section('content')
-    <!-- Iframe ini hanya akan muncul saat menu UTS diklik -->
-    <iframe src="{{ asset('Pendaftaran/form.html') }}" width="100%" height="500" frameborder="0"></iframe>
+    <iframe src="{{ url('/load-form-uts') }}" width="100%" height="500" frameborder="0"></iframe>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,15 @@ Route::get('/', function () {
     return view('welcome'); // Membuka file welcome.blade.php
 });
 
-// 2. Route untuk Halaman Komik (Jika mau diakses terpisah)
+// 2. Route untuk Halaman Komik
 Route::get('/komik', function () {
     return view('menu.komik'); // Membuka file komik.blade.php yang ada di dalam folder menu
 });
 
 Route::get('/uts', function () {
     return view('menu.uts');
+});
+// route khusus untuk memuat form HTML
+Route::get('/load-form-uts', function () {
+    return file_get_contents(resource_path('views/Pendaftaran/form.html'));
 });
